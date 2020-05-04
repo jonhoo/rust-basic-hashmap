588273c5a35cbd5d56b0eb4e2ac70d86d1326dd5
morazow <m.orazow@gmail.com>
2020-05-04 07:35:48 +0200

    Apply review suggestions from @jonhoo.

--- src/lib.rs
+++ src/lib.rs
@@ -78,14 +78,17 @@ impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq,
 {
-    fn bucket<Q>(&self, key: &Q) -> usize
+    fn bucket<Q>(&self, key: &Q) -> Option<usize>
     where
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
     {
+        if self.buckets.len() == 0 {
+            return None;
+        }
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
-        (hasher.finish() % self.buckets.len() as u64) as usize
+        Some((hasher.finish() % self.buckets.len() as u64) as usize)
     }
 
     pub fn entry<'a>(&'a mut self, key: K) -> Entry<'a, K, V> {
@@ -93,7 +96,7 @@ where
             self.resize();
         }
 
-        let bucket = self.bucket(&key);
+        let bucket = self.bucket(&key).unwrap();
         match self.buckets[bucket].iter().position(|&(ref ekey, _)| ekey == &key) {
             Some(index) => Entry::Occupied(OccupiedEntry {
                 entry: &mut self.buckets[bucket][index]
@@ -107,7 +110,7 @@ where
             self.resize();
         }
 
-        let bucket = self.bucket(&key);
+        let bucket = self.bucket(&key).unwrap();
         let bucket = &mut self.buckets[bucket];
 
         for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
@@ -126,10 +129,7 @@ where
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
     {
-        if self.is_empty() {
-            return None;
-        }
-        let bucket = self.bucket(key);
+        let bucket = self.bucket(key)?;
         self.buckets[bucket]
             .iter()
             .find(|&(ref ekey, _)| ekey.borrow() == key)
@@ -141,11 +141,7 @@ where
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
     {
-        if self.is_empty() {
-            false
-        } else {
-            self.get(key).is_some()
-        }
+        self.get(key).is_some()
     }
 
     pub fn remove<Q>(&mut self, key: &Q) -> Option<V>
@@ -153,10 +149,7 @@ where
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
     {
-        if self.is_empty() {
-            return None;
-        }
-        let bucket = self.bucket(key);
+        let bucket = self.bucket(key)?;
         let bucket = &mut self.buckets[bucket];
         let i = bucket
             .iter()
